278182112ca79229a087bf5f212516a23ab10f8d
Eren Kafadar <kafadare@pennmedicine.upenn.edu>
2023-09-07 15:02:34 -0400

    debug echo

--- Rstudio_server.sh
+++ Rstudio_server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $PATH
 space="$1" #in GB
 time="$2" #in hours
 qrsh -l h_data=${space},h_rt=${time}:00:00
@@ -7,8 +6,10 @@ qrsh -l h_data=${space},h_rt=${time}:00:00
 mkdir -pv $SCRATCH/rstudiotmp/var/lib
 mkdir -pv $SCRATCH/rstudiotmp/var/run
 mkdir -pv $SCRATCH/rstudiotmp/tmp
+echo "dir created"
 #Setup apptainer
 module load apptainer
+echo "module load"
 #Run rstudio
 apptainer run -B $SCRATCH/rstudiotmp/var/lib:/var/lib/rstudio-server -B $SCRATCH/rstudiotmp/var/run:/var/run/rstudio-server -B $SCRATCH/rstudiotmp/tmp:/tmp $H2_CONTAINER_LOC/h2-rstudio_4.1.0.sif
 # This command will display some information and a `ssh -L ...` command for you to run on a separate terminal
