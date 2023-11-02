f60b4eb8233e4f036ba6e3e3d0e031f4eda44694
Eren Kafadar <kafadare@pennmedicine.upenn.edu>
2023-11-02 19:38:11 -0400

    edit genotypes script

--- rm_geno_out.sh
+++ rm_geno_out.sh
@@ -4,17 +4,19 @@
 #$ -j y
 #$ -o /u/home/k/kafadare/project-gandalm/mesc_genExp_scores/
 #$ -m a
-#$ -t 1-22
+#$ -t 1-2
 
 CHR=${SGE_TASK_ID}
 
-expr = /u/home/k/kafadare/project-gandalm/genExp_batches
+geno="/u/home/k/kafadare/project-gandalm/merged_genotype_Chr/merged_chr${CHR}" ##file determined by which chromosome
 
-geno=/u/home/k/kafadare/project-gandalm/merged_genotype_Chr/XXX ##file determined by which chromosome
+keep_file="/u/home/k/kafadare/project-gandalm/comb_data/new_data/processed/genoID_1968.txt"
 
-cov=/u/home/k/kafadare/project-gandalm/comb_data/new_data/processed/
+outdir="/u/home/k/kafadare/project-gandalm/merged_genotype_Chr/merged_chr_filtered${CHR}"
 
-rel_file=/u/project/gandalm/cindywen/isoform_twas/genotype/all_data/isec_R2_greater_than_3/ancestry/related.txt
+. /u/local/Modules/default/init/modules.sh
+module load plink
 
-outdir=/u/project/gandalm/cindywen/isoform_twas/eqtl_new/results/mixed_nominal_${num_hcp}hcp
+plink --bfile ${geno} --keep ${keep_file} --make-bed --out ${outdir}
+
 
